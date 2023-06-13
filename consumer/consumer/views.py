257967cba00f1c073a498226d1245f83d45b7dcb
Hiansdt <vinijao123690@gmail.com>
2023-06-13 15:15:20 -0300

    first commit

---
+++ consumer/consumer/views.py
@@ -0,0 +1,48 @@
+from kafka import KafkaProducer, KafkaConsumer
+from django.http import HttpResponse
+
+def my_view(request):
+    # Create a Kafka producer
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    # Send a message to a Kafka topic
+    topic = 'my_topic'
+    message = 'Hello, Kafka!'
+    producer.send(topic, message.encode('utf-8'))
+
+    # Other view code...
+
+    return HttpResponse('Message sent to Kafka')
+
+def send_message(request):
+    # Create a Kafka producer
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    # Send a message to a Kafka topic
+    topic = 'my_topic'
+    message = 'Hello, Kafka!'
+    producer.send(topic, message.encode('utf-8'))
+
+    return HttpResponse('Message sent to Kafka')
+
+def receive_message(request):
+    # Create a Kafka consumer
+    consumer = KafkaConsumer(
+        'my_topic',
+        bootstrap_servers='localhost:9092',
+        group_id='my_consumer_group',
+        auto_offset_reset='earliest',
+        enable_auto_commit=False,
+        value_deserializer=lambda x: x.decode('utf-8')
+    )
+
+    # Consume messages from the Kafka topic
+    messages = []
+
+    consumer.close()
+
+    # Display the consumed messages on the screen
+    response = "<br>".join(messages)
+    return HttpResponse(response)
+
+
